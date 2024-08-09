5ac54db1ac19e141616b2ef2142fc1596c946537
jizhaoqin <jizhao.qin.de@gmail.com>
2024-08-09 17:23:34 +0200

    build: minimal kernel, after recording

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,21 @@
 
 use core::panic::PanicInfo;
 
+static HELLO: &[u8] = b"Hello World and Bilibili! And byebye!!!!!";
+
 #[no_mangle] // 不重整函数名
 pub extern "C" fn _start() -> ! {
     // 因为链接器会寻找一个名为 `_start` 的函数，所以这个函数就是入口点
     // 默认命名为 `_start`
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
 
