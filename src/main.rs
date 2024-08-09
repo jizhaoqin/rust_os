508b33bc3aba3ee37773ead53e602d3e436c63bf
jizhaoqin <jizhao.qin.de@gmail.com>
2024-08-09 20:27:47 +0200

    build: VGA text mode, before recording

--- src/main.rs
+++ src/main.rs
@@ -3,26 +3,20 @@
 
 use core::panic::PanicInfo;
 
-static HELLO: &[u8] = b"Hello World and Bilibili! And byebye!!!!!";
+mod vga_buffer;
 
 #[no_mangle] // 不重整函数名
 pub extern "C" fn _start() -> ! {
-    // 因为链接器会寻找一个名为 `_start` 的函数，所以这个函数就是入口点
-    // 默认命名为 `_start`
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
-
+    // 因为链接器会寻找一个默认名为 `_start` 的函数，所以这个函数就是入口点
+    println!("Hello World{}", "!");
+    println!("Hello again!");
+    // panic!("Some panic message");
     loop {}
 }
 
 /// 这个函数将在 panic 时被调用
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    print!("{}", info);
     loop {}
 }
