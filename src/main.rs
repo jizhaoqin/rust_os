2adbcb7d09e26add783b0b885585ad04304945dd
jizhaoqin <jizhao.qin.de@gmail.com>
2024-08-13 21:46:30 +0200

    build: testing, before recording

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,32 @@
 #![no_std] // 不链接 Rust 标准库
 #![no_main] // 禁用所有 Rust 层级的入口点
+#![feature(custom_test_frameworks)]
+#![test_runner(rust_os::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
-
-mod vga_buffer;
+use rust_os::println;
 
 #[no_mangle] // 不重整函数名
 pub extern "C" fn _start() -> ! {
-    // 因为链接器会寻找一个默认名为 `_start` 的函数，所以这个函数就是入口点
     println!("Hello World{}", "!");
     println!("Hello again!");
-    // panic!("Some panic message");
+
+    #[cfg(test)]
+    test_main();
+
     loop {}
 }
 
-/// 这个函数将在 panic 时被调用
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    print!("{}", info);
+    println!("{}", info);
     loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    rust_os::test_panic_handler(info);
 }
---
+++ tests/basic_boot.rs
@@ -0,0 +1,42 @@
+#![no_std]
+#![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(rust_os::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+use core::panic::PanicInfo;
+use rust_os::println;
+use rust_os::vga_buffer::WRITER;
+
+#[no_mangle] // don't mangle the name of this function
+pub extern "C" fn _start() -> ! {
+    test_main();
+
+    loop {}
+}
+
+#[test_case]
+fn test_println_simple() {
+    println!("test_println_simple output");
+}
+
+#[test_case]
+fn test_println_many() {
+    for _ in 0..200 {
+        println!("test_println_many output");
+    }
+}
+
+#[test_case]
+fn test_println_output() {
+    let s = "Some test string that fits on a single line";
+    println!("{}", s);
+    for (i, c) in s.chars().enumerate() {
+        assert_eq!(WRITER.lock().get_screen_char(i), c);
+    }
+}
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    rust_os::test_panic_handler(info)
+}
