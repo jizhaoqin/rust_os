5219d136e43bb0dc4811765336a0bd71d0e41914
jizhaoqin <jizhao.qin.de@gmail.com>
2024-10-24 14:27:29 +0200

    build: memory paging impl (2), after recording

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,11 @@
 
 use bootloader::{entry_point, BootInfo};
 use core::panic::PanicInfo;
-use rust_os::println;
+use rust_os::{print, println};
 
 entry_point!(kernel_main);
 
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
-    // 链接器会寻找一个默认名为 `_start` 的函数，所以这个函数就是入口点
     println!("Hello World{}", "!");
 
     rust_os::init();
@@ -67,39 +66,80 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
     //     level_4_page_table.start_address()
     // );
 
-    use rust_os::memory::active_level_4_table;
-    use x86_64::structures::paging::PageTable;
-    use x86_64::VirtAddr;
+    // use rust_os::memory::active_level_4_table;
+    // use x86_64::structures::paging::PageTable;
 
-    let mut count = 0;
-    // 看看4, 3级页表,
+    // let mut count = 0;
+    // // 看看4, 3级页表,
+    // let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    // let l4_table = unsafe { active_level_4_table(phys_mem_offset) };
+
+    // for (i, entry) in l4_table.iter().enumerate() {
+    //     if !entry.is_unused() {
+    //         println!("L4 Entry {}: {:?}", i, entry);
+
+    //         // get the physical address from the entry and convert it
+    //         let phys = entry.frame().unwrap().start_address();
+    //         let virt = phys.as_u64() + boot_info.physical_memory_offset;
+    //         let ptr = VirtAddr::new(virt).as_mut_ptr();
+    //         let l3_table: &PageTable = unsafe { &*ptr };
+
+    //         // print non-empty entries of the level 3 table
+    //         for (i, entry) in l3_table.iter().enumerate() {
+    //             if !entry.is_unused() {
+    //                 println!("  L3 Entry {}: {:?}", i, entry);
+    //                 count += 1;
+    //             }
+    //         }
+    //     }
+    // }
+
+    // 让我们通过翻译一些地址来测试我们的翻译功能
+    // use rust_os::memory;
+    // use x86_64::{structures::paging::Translate, VirtAddr};
+
+    // let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    // // new: initialize a mapper
+    // let mapper = unsafe { memory::init(phys_mem_offset) };
+
+    // let addresses = [
+    //     // the identity-mapped vga buffer page
+    //     0xb8000,
+    //     // some code page
+    //     0x201008,
+    //     // some stack page
+    //     0x0100_0020_1a10,
+    //     // virtual address mapped to physical address 0
+    //     boot_info.physical_memory_offset,
+    // ];
+
+    // for &address in &addresses {
+    //     let virt = VirtAddr::new(address);
+    //     let phys =  mapper.translate_addr(virt);
+    //     println!("{:?} -> {:?}", virt, phys);
+    // }
+
+    // 创建映射
+    use rust_os::memory;
+    use x86_64::{structures::paging::Page, VirtAddr};
+
     let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
-    let l4_table = unsafe { active_level_4_table(phys_mem_offset) };
+    let mut mapper = unsafe { memory::init(phys_mem_offset) };
+    let mut frame_allocator = memory::EmptyFrameAllocator;
 
-    for (i, entry) in l4_table.iter().enumerate() {
-        if !entry.is_unused() {
-            println!("L4 Entry {}: {:?}", i, entry);
+    // 映射未使用的页
+    // let page = Page::containing_address(VirtAddr::new(0));
+    let page = Page::containing_address(VirtAddr::new(0xdeadbeaf000));
+    memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
 
-            // get the physical address from the entry and convert it
-            let phys = entry.frame().unwrap().start_address();
-            let virt = phys.as_u64() + boot_info.physical_memory_offset;
-            let ptr = VirtAddr::new(virt).as_mut_ptr();
-            let l3_table: &PageTable = unsafe { &*ptr };
-
-            // print non-empty entries of the level 3 table
-            for (i, entry) in l3_table.iter().enumerate() {
-                if !entry.is_unused() {
-                    println!("  L3 Entry {}: {:?}", i, entry);
-                    count += 1;
-                }
-            }
-        }
-    }
+    // 通过新的映射将字符串 `New!`  写到屏幕上。
+    let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
+    unsafe { page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e) };
 
     #[cfg(test)]
     test_main();
 
-    println!("It did not crash! {}", count);
+    print!("It did not crash!");
     rust_os::hlt_loop();
 }
 
--- src/memory.rs
+++ src/memory.rs
@@ -1,7 +1,22 @@
-use x86_64::{structures::paging::PageTable, VirtAddr};
+use x86_64::{
+    structures::paging::{
+        FrameAllocator, Mapper, OffsetPageTable, Page, PageTable, PhysFrame, Size4KiB,
+    },
+    PhysAddr, VirtAddr,
+};
+
+/// 初始化一个新的OffsetPageTable。
+///
+/// 这个函数是不安全的，因为调用者必须保证完整的物理内存在
+/// 传递的`physical_memory_offset`处被映射到虚拟内存。另
+/// 外，这个函数必须只被调用一次，以避免别名"&mut "引用（这是未定义的行为）。
+pub unsafe fn init(physical_memory_offset: VirtAddr) -> OffsetPageTable<'static> {
+    let level_4_table = active_level_4_table(physical_memory_offset);
+    OffsetPageTable::new(level_4_table, physical_memory_offset)
+}
 
 /// 返回一个对活动的4级表的可变引用。
-pub unsafe fn active_level_4_table(physical_memory_offset: VirtAddr) -> &'static mut PageTable {
+unsafe fn active_level_4_table(physical_memory_offset: VirtAddr) -> &'static mut PageTable {
     use x86_64::registers::control::Cr3;
 
     let (level_4_table_frame, _) = Cr3::read();
@@ -11,4 +26,31 @@ pub unsafe fn active_level_4_table(physical_memory_offset: VirtAddr) -> &'static
     let page_table_ptr: *mut PageTable = virt.as_mut_ptr();
 
     &mut *page_table_ptr // unsafe
+}
+
+/// 为给定的页面创建一个实例映射到框架`0xb8000`。
+pub fn create_example_mapping(
+    page: Page,
+    mapper: &mut OffsetPageTable,
+    frame_allocator: &mut impl FrameAllocator<Size4KiB>,
+) {
+    use x86_64::structures::paging::PageTableFlags as Flags;
+
+    let frame = PhysFrame::containing_address(PhysAddr::new(0xb8000));
+    let flags = Flags::PRESENT | Flags::WRITABLE;
+
+    let map_to_result = unsafe {
+        // FIXME: 这并不安全，我们这样做只是为了测试。
+        mapper.map_to(page, frame, flags, frame_allocator)
+    };
+    map_to_result.expect("map_to failed").flush();
+}
+
+/// 一个总是返回`None'的FrameAllocator。
+pub struct EmptyFrameAllocator;
+
+unsafe impl FrameAllocator<Size4KiB> for EmptyFrameAllocator {
+    fn allocate_frame(&mut self) -> Option<PhysFrame> {
+        None
+    }
 }
