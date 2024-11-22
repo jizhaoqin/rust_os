7625209afaf67a4679abd85c80222acaa92cfda9
jizhaoqin <jizhao.qin.de@gmail.com>
2024-11-22 21:04:30 +0100

    build: heap allocation (2), after recording

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ extern crate alloc;
 
 use bootloader::{entry_point, BootInfo};
 use core::panic::PanicInfo;
-use rust_os::{memory, print, println};
+use rust_os::{print, println};
 
 entry_point!(kernel_main);
 
@@ -138,9 +138,40 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
     // let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
     // unsafe { page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e) };
 
-    use alloc::boxed::Box;
+    use alloc::{boxed::Box, rc::Rc, vec, vec::Vec};
+    use rust_os::allocator;
+    use rust_os::memory::{self, BootInfoFrameAllocator};
+    use x86_64::VirtAddr;
 
-    let _x = Box::new(42);
+    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let mut mapper = unsafe { memory::init(phys_mem_offset) };
+    let mut frame_allocator = unsafe { BootInfoFrameAllocator::init(&boot_info.memory_map) };
+
+    allocator::init_heap(&mut mapper, &mut frame_allocator).expect("heap initialization failed");
+
+    // allocate a number on the heap
+    let heap_value = Box::new(42);
+    println!("heap_value at {:p}", heap_value);
+
+    // create a dynamically sized vector
+    let mut vec = Vec::new();
+    for i in 0..500 {
+        vec.push(i);
+    }
+    println!("vec at {:p}", vec.as_slice());
+
+    // create a reference counted vector -> will be freed when count reaches 0
+    let reference_counted = Rc::new(vec![1, 2, 3]);
+    let cloned_reference = reference_counted.clone();
+    println!(
+        "current reference count is {}",
+        Rc::strong_count(&cloned_reference)
+    );
+    core::mem::drop(reference_counted);
+    println!(
+        "reference count is {} now",
+        Rc::strong_count(&cloned_reference)
+    );
 
     #[cfg(test)]
     test_main();
