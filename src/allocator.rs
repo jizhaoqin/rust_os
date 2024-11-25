5d41800a164513f2583fa29c21f3ffab0f4c9bcd
jizhaoqin <jizhao.qin.de@gmail.com>
2024-11-25 17:29:25 +0100

    build: allocator design (3), fixed block allocator, before recording

--- src/allocator.rs
+++ src/allocator.rs
@@ -1,18 +1,23 @@
 // use bump::BumpAllocator;
+// use dummy::Dummy;
+use fixed_size_block::FixedSizeBlockAllocator;
+// use linked_list::LinkedListAllocator;
 // use linked_list_allocator::LockedHeap;
-use linked_list::LinkedListAllocator;
 use x86_64::structures::paging::mapper::MapToError;
 use x86_64::structures::paging::{FrameAllocator, Mapper, Page, PageTableFlags, Size4KiB};
 use x86_64::VirtAddr;
 
 pub mod bump;
 pub mod dummy;
+pub mod fixed_size_block;
 pub mod linked_list;
 
 #[global_allocator]
-static ALLOCATOR: Locked<LinkedListAllocator> = Locked::new(LinkedListAllocator::new());
+static ALLOCATOR: Locked<FixedSizeBlockAllocator> = Locked::new(FixedSizeBlockAllocator::new());
+// static ALLOCATOR: Locked<LinkedListAllocator> = Locked::new(LinkedListAllocator::new());
 // static ALLOCATOR: Locked<BumpAllocator> = Locked::new(BumpAllocator::new());
 // static ALLOCATOR: LockedHeap = LockedHeap::empty();
+// static ALLOCATOR: Dummy = Dummy;
 
 pub const HEAP_START: usize = 0x_4444_4444_0000;
 pub const HEAP_SIZE: usize = 100 * 1024; // 100 KiB
